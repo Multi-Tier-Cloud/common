b74cb4185c55940ad7921e81a1bb1a74511e0dc8
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-30 10:56:57 -0400

    Added tests for psk flags and generation functions   - Should be able to close #7

--- util/psk.go
+++ util/psk.go
@@ -1,3 +1,17 @@
+/* Copyright 2020 Multi-Tier-Cloud Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package util
 
 import (
@@ -14,6 +28,9 @@ import (
 // possibly reduce readability and comprehension of the underlying type.
 type pskValue pnet.PSK
 
+// libp2p's PSK definition is a slice of 32 bytes
+const PSK_NUM_BYTES = 32
+
 var (
 	// Stores the bootstrap multiaddrs
 	psk pskValue
@@ -26,8 +43,8 @@ var (
 
 // Generates a random PSK
 func CreateRandPSK() (pnet.PSK, error) {
-	randBytes := make([]byte, 32)
-	if size, err := rand.Read(randBytes); size != 32 || err != nil {
+	randBytes := make([]byte, PSK_NUM_BYTES)
+	if size, err := rand.Read(randBytes); size != PSK_NUM_BYTES || err != nil {
 		return nil, fmt.Errorf("Unable to generate random bytes")
 	}
 
@@ -95,4 +112,11 @@ func AddPSKFlag() (*pnet.PSK, error) {
 
 	// Cast and return
 	return (*pnet.PSK)(&psk), nil
+}
+
+// For enabling tests, ideally should not be used.
+// This is needed to return a pointer to type pskValue, a hidden type.
+// This enables tests for the Set() and String() functions above.
+func GetPSKPointer() *pskValue {
+	return &psk
 }
